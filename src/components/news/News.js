5a01ea309fdc0a4866b437d98ed9b91c55e59adf
Yasir Khurshid <yasir.khurshid@gmail.com>
2018-07-27 23:24:41 +0200

    Add news component

---
+++ src/components/news/News.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+import NewSingle from './NewSingle'
+
+class News extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            news: []
+        }
+    }
+
+    componentDidMount() {
+        const url = 'https://newsapi.org/v2/'+this.props.newsType+'?'+this.props.query+'&pageSize=20&apiKey=542f3217acf34a3f8e96960322857fe1';
+
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({
+                    news: data.articles
+                })
+            })
+            .catch((error) => console.log(error));
+
+    }
+
+    renderItems() {
+        return this.state.news.filter((i) => i.urlToImage !== null).map((item) => (
+            <NewSingle key={item.url} item={item}/>
+        ))
+    }
+
+    render() {
+        return (
+            <div className="row">
+                {this.renderItems()}
+            </div>
+        )
+    }
+}
+
+export default News
